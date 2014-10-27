c1134ecc21908ea052674a19010c33b67c28b7f1
Nathaniel Brockman <brockman@air-brockman-2013.local>
2014-10-27 01:37:33 -0400

    Added a working INSERT that uses symbols

--- python/ingress_nx2.py
+++ python/ingress_nx2.py
@@ -9,6 +9,22 @@ from pprint import pprint
 from datetime import date, datetime, timedelta
 from geopy.point import Point
 
+nmeaDict = {
+    "XTE" : "Cross Track Error",
+    "WCV" : "Waypoint Velocity",
+    "HDT" : "Heading True", # <HDT(heading=Decimal('261'), hdg_true='T')>
+    "HDM" : "Heading Magnetic",
+    "MWD" : "Measured Wind Direction", # <MWD(direction_true=Decimal('231.14'), true='T', direction_magnetic=Decimal('238.44'), magnetic='M', wind_speed_knots=Decimal('4.35'), knots='N', wind_speed_meters=Decimal('2.24'), meters='M')>
+    "VHW" : "Velocity Headway", # <VHW(heading_true=Decimal('261'), true='T', heading_magnetic=Decimal('268'), magnetic='M', water_speed_knots=Decimal('6.46'), knots='N', water_speed_km=Decimal('11.96'), kilometers='K')> 
+    "DBT" : "Depth Below Transducer",
+    "GLL" : "GPS Position LatLon",
+    "GSV" : "GPS Sattelites in View",
+    "ZDA" : "GPS Date Time",
+    "VTG" : "Velocity Over Ground",
+    "MWV" : "Measured Wind Velocity"
+}
+
+
 
 if len(sys.argv) < 3:
     print "Usage: ingress_nx2.py <captureDate YYYY-MM-DD> <nx2LogFilename>"
@@ -70,26 +86,43 @@ for lineRaw in readf:
       print "Last Point:  "
       pprint(point)
 
-      csv_row = []
-      r = csv.reader([line])
-      for row in r:
-        csv_row = row
+      paramNames = ""
+      paramValues = ""
 
-        # Dynmically build the parameter list. , p0s, p0f,  csv_row[1], float(csv_row[1])    
-        paramNames = ""
-        paramValues = ""
-        for x in range(1, len(csv_row)):
-          paramValues += "'%s'," % csv_row[x]
-          paramNames += "p%ds," % x
-          # does it blend?
-          try:
-            paramValues += "%f," % float(csv_row[x])
-            paramNames += "p%df," % x
-          except ValueError:
-            1 # supress exception
+## The dictionary method is awesome
+# <WCV(velocity='-7.17', vel_units='N', waypoint_id='046') data=['A']>
+      mD = msg.name_to_idx
+      for k in mD.keys():
+        v = msg.data[mD[k]]
 
-        print "INSERT INTO nmeas ( last_gps_on, last_gps, last_lat, last_lon, sentence, %s noun ) values ( '%s', point(%f,%f), %f, %f, '%s', %s '%s' );\n" % ( paramNames, last_on, point.latitude, point.longitude, point.latitude, point.longitude, line, paramValues,csv_row[0] )
+        if not k == "true" and not k == "timestamp":
+          if not v.rstrip() == "":
+            # does it blend?
+            try:
+              paramValues += "%f," % float(v)
+              paramNames += "%s," % k
+            except ValueError:
+              paramValues += '"%s",' % v
+              paramNames += "%s," % k
+     
+## The CSV row method is clunky, lets use the NMEA parser to its full power!
+#      csv_row = []
+#      r = csv.reader([line])
+#      for row in r:
+#        csv_row = row
+#        # Dynmically build the parameter list. , p0s, p0f,  csv_row[1], float(csv_row[1])    
+#        for x in range(1, len(csv_row)):
+#          paramValues += "'%s'," % csv_row[x]
+#          paramNames += "p%ds," % x
+#          # does it blend?
+#          try:
+#            paramValues += "%f," % float(csv_row[x])
+#            paramNames += "p%df," % x
+#          except ValueError:
+#            1 # supress exception
 
+
+      print "INSERT INTO nmeas ( last_gps_on, last_gps, last_lat, last_lon, nmea_sentence, %s noun, description ) values ( '%s', point(%f,%f), %f, %f, '%s', %s '%s', '%s' );\n" % ( paramNames, last_on, point.latitude, point.longitude, point.latitude, point.longitude, line, paramValues, msg.__class__.__name__, nmeaDict[msg.__class__.__name__] )
 
 
       ## Store th values for the next loop
@@ -100,3 +133,13 @@ for lineRaw in readf:
       lastLon = lon
 
 readf.close()
+
+
+#
+# ^o^
+#       |\    ship it!
+#       | \   /
+#      /|  \
+#   __/_|___\_
+# ~~\________/~~
+#  ~~~~~~~~~~~~
---
+++ python/insertAll.sh
@@ -0,0 +1,1 @@
+./ingress_nx2.py 2014-04-27 ../incoming/data/COM6_nx2_NMEA.log |grep INSERT|../mysql/connect.sh
